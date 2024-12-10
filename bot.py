000ffddd6df49c492a6274287e93c1b17805961b
Ivan <101109181+FaraamFide@users.noreply.github.com>
2024-12-10 22:03:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,12 +27,3 @@ if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
 
 
-
-# CALLBACK DATA EXAMPLE
-#{"id": "3300290482092852398", "from": {"id": 12378483456 "is_bot": false, "first_name": "Иван", "username": "faraamf", "language_code": "en"},
-#  "message": {"message_id": 1171, "from": {"id": 8151232315, "is_bot": true, "first_name": "Library", "username": "russian_library_bot"}, 
-# "chat": {"id": 41234513467, "first_name": "Иван", "username": "faraamf", "type": "private"},
-#  "date": 1730643784, "text": "Произведения Пушкин А. С.:", "reply_markup": {"inline_keyboard": {"text": "19 октября 1827", "callback_data": "work_/author/pushkin/text/629/p.1"},
-#  {"text": "19 октября 1828", "callback_data": "work_/author/pushkin/text/658/p.1"}, {"text": "19 октября", "callback_data": "work_/author/pushkin/text/566/p.1"}, 
-# {"text": "27 мая 1819", "callback_data": "work_/author/pushkin/text/387/p.1"}, {"text": "«A son amant Eglé sans résistance...»", "callback_data": "work_/author/pushkin/text/423/p.1"},
-#  {"text": "➡️ Следующая страница", "callback_data": "page_pushkin_1"}}}, "chat_instance": "-1457524254077791789", "data": "work_/author/pushkin/text/629/p.1"}
