abc14245f164cd71d1af4bfcd1b92b28d6e8e879
Ivan <101109181+FaraamFide@users.noreply.github.com>
2024-12-15 23:17:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,12 +3,8 @@ from config import API_TOKEN
 from handlers import register_handlers
 import atexit
 
-from data_store import load_data, save_data#, mes_to_execute_and_position
-# Так как программа работает не постоянно, нужно добавить сторонний json файл чтобы избежать  if messages_to_execute[user_id]["work"] != []: KeyError: (тут ID пользователя)
-# Это возникает когда программа запускается дважды а user_id назначается при старте, хотя юзер делал это при первом запуске. В будующем это можно будет убрать, если прога работает 24/7
-# Хотя лучше оставить чтобы иметь возможность делать обновления
+from data_store import load_data, save_data
 
-# Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN, parse_mode="MarkdownV2")
 dp = Dispatcher(bot)
 
@@ -16,7 +12,7 @@ dp = Dispatcher(bot)
 
 load_data()
 
-# Регистрация всех обработчиков
+
 register_handlers(dp)
 
 
