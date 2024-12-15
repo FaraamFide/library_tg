5fdbdb1d2deccbec037743b9cfbb5ca97b0466dc
Ivan <101109181+FaraamFide@users.noreply.github.com>
2024-12-15 23:17:44 +0300

    Update data_store.py

--- data_store.py
+++ data_store.py
@@ -2,20 +2,16 @@ import json
 
 DATA_FILE = "data.json"
 
-# Глобальный словарь для хранения данных
-#mes_to_execute_and_position = {}
-
 def load_data():
-    #global mes_to_execute_and_position
     try:
         with open(DATA_FILE, "r", encoding="utf-8") as file:
             mes_to_execute_and_position = json.load(file)
-            #print("Loaded", mes_to_execute_and_position.keys())
+
     except (FileNotFoundError, json.JSONDecodeError):
         mes_to_execute_and_position = {}
         print("JSON file not found or corrupted. Initialized with empty data.")
 
-    return mes_to_execute_and_position #$#
+    return mes_to_execute_and_position 
 
 def save_data():
     with open(DATA_FILE, "w", encoding="utf-8") as file:
