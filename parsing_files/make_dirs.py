a112a31e13bee4071465ff15daa3441ad13ea336
Ivan <101109181+FaraamFide@users.noreply.github.com>
2024-12-15 23:23:29 +0300

    Update make_dirs.py

--- parsing_files/make_dirs.py
+++ parsing_files/make_dirs.py
@@ -10,8 +10,6 @@ if __name__ == "__main__":
 
     for author in authors.split('\n'):
         a = author.split(' -- ')
-        #a[1] = a[1].replace(u'\xa0', u' ')
-        #a[1] = a[1].replace(u'.', u'')
         print(a[0])
         
         os.makedirs(f"tg_library/books/{a[0]}", exist_ok=True)
